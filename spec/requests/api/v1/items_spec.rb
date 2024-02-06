e249788e5bd70657de64bbead330322bf8350a67
Sam Puttman <daputter2@gmail.com>
2024-02-06 17:12:58 -0500

    Add tests for merchant and item show

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -14,8 +14,27 @@ RSpec.describe "Api::V1::Items", type: :request do
 
       expect(json_response["data"].size).to eq(3)
       expect(json_response["data"].size).not_to eq(nil)
-      expect(json_response["data"].first["attributes"]).to include("id", "name", "description", "unit_price", "merchant_id")
-      expect(json_response["data"].last["attributes"]).to include("id", "name", "description", "unit_price", "merchant_id")
+      expect(json_response["data"].first["attributes"]).to include("id", "name", "description", "unit_price")
+      expect(json_response["data"].last["attributes"]).to include("id", "name", "description", "unit_price")
+    end
+  end
+
+  describe "GET /api/v1/items/:id" do
+    let!(:item) { create(:item) }
+
+    it "returns specified item attributes" do
+      get api_v1_item_path(item)
+
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+
+      expect(json_response["data"]["id"]).to eq(item.id.to_s)
+      expect(json_response["data"]["type"]).to eq("item")
+      expect(json_response["data"]["attributes"]["name"]).to eq(item.name)
+      expect(json_response["data"]["attributes"]["description"]).to eq(item.description)
+      expect(json_response["data"]["attributes"]["unit_price"]).to eq(item.unit_price)
+
+      expect(json_response["data"]["attributes"]).not_to have_key("merchant_id")
     end
   end
 end
