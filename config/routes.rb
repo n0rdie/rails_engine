94078e6a0869d19522c379d3db5b99cef6f130a3
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-08 17:37:53 -0700

    Remove outer PATCH route and create with resource - to make Postman tests pass (uses PUT in tests)

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  get "api/v1/items/find", to: "api/v1/items#find"
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      resources :items, only: [:index, :show, :create, :destroy]
+      resources :merchants, only: [:index, :show] do 
+        resources :items, only: [:index]
+      end
+      resources :items, only: [:index, :show, :update, :create, :destroy]
     end
   end
-
-  patch "/api/v1/items/:id", to: "api/v1/items#update"
 end
