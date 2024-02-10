adcc0bc9e843466a6a93a06b44e9341047cabaab
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-09 19:14:47 -0700

    Add data attribute to ErrorMessage to fulfill client request to have additional data returned for merchant sad path

--- app/poros/error_message.rb
+++ app/poros/error_message.rb
@@ -1,9 +1,10 @@
 class ErrorMessage
-    attr_reader :message, :status_code
+    attr_reader :message, :status_code, :data
   
-    def initialize(message, status_code)
+    def initialize(message, status_code, data = [])
         @message = message
         @status_code = status_code
+        @data = data
     end
   
 end
